be596e3fa5c1ff95dfcac4c74135f245171d134d
Ildephonse BIKINO <ildephonsebikino@Ildephonses-MacBook-Pro.local>
2020-10-21 18:50:11 -0400

    First commit

---
+++ src/main/java/com/bigcart/authenticationserver/BigcartAuthenticationServerApplication.java
@@ -0,0 +1,13 @@
+package com.bigcart.authenticationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigcartAuthenticationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigcartAuthenticationServerApplication.class, args);
+    }
+
+}
